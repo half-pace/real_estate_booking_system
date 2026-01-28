f995d032053232a36dcd8a89fcfc74230977a3c3
half pace <halfpace64@gmail.com>
2026-01-29 00:29:32 +0530

    Done with userRepo layout

--- data/repositories/user_repo.py
+++ data/repositories/user_repo.py
@@ -1,1 +1,17 @@
 
+class UserRepo:
+    def __init__(self, storage):
+        self.storage = storage
+        
+    #methods
+    def add_user(self):
+        pass
+    
+    def get_user_by_id(self):
+        pass
+    
+    def update_user(self):
+        pass
+    
+    def delete_user(self):
+        pass
